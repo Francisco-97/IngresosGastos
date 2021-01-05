a12c21cd80a8f89bfd4a51f67001e88ed8579d74
Francisco <Johnkelsantos@gmail.com>
2021-01-05 13:02:34 -0400

    Primer commit

---
+++ IngresosGastos/Models/IngresosGastosFASS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IngresosGastos.Models
+{
+    public class IngresosGastosFASS
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength (60, MinimumLength = 3)]
+        public string Descripcion { get; set; }
+        [Required]
+        public bool EsIngresos { get; set; }
+        [Required]
+        public double valor { get; set; }
+    }
+}
